3b9cc1c8844fd93d43207bb77b73b1d51d826720
dhruval <dhruval16kush@gmail.com>
2023-06-07 13:21:29 +0530

    cors addition

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 require("dotenv").config();
 
 const userRoutes = require("./routes/userRoutes");
@@ -8,6 +8,7 @@ const orderRoutes = require("./routes/orderRoutes");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 mongoose
   .connect(process.env.MONGO_URL)
